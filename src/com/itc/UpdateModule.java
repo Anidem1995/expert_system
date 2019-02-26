f4ddf1cc38fd0d323810e11fc7b914f793585f81
José Antonio Medina Quintana <medina.octubre95@live.com.mx>
2019-02-26 00:00:47 -0600

    Minor updates on Justification Module

--- src/com/itc/UpdateModule.java
+++ src/com/itc/UpdateModule.java
@@ -11,6 +11,11 @@ public class UpdateModule {
     FileManagement fileManagement;
     List<Record> knowledge_base;
 
+    public void addRule() {
+        Record record = setRule();
+        fileManagement.write(record);
+    }
+
     public void updateRule() {
         fileManagement = new FileManagement();
         JustificationModule justificationModule = new JustificationModule();
@@ -23,209 +28,7 @@ public class UpdateModule {
         System.out.println("Ingresa el número de la regla a modificar");
         int rule_number = scanner.nextInt();
 
-        System.out.println("Numero de antecedentes");
-        int a = scanner.nextInt();
-        String n;
-        Record record = new Record();
-        switch (a){
-            case 1:
-                System.out.println("Antecedente 1");
-                n = scanner.nextLine();
-                n = scanner.nextLine();
-                record.setA1(n);
-                break;
-            case 2:
-                System.out.println("Antecedente 1");
-                n = scanner.nextLine();
-                n = scanner.nextLine();
-                record.setA1(n);
-                System.out.println("Antecedente 2");
-                n = scanner.nextLine();
-                record.setA2(n);
-                break;
-            case 3:
-                System.out.println("Antecedente 1");
-                n = scanner.nextLine();
-                n = scanner.nextLine();
-                record.setA1(n);
-                System.out.println("Antecedente 2");
-                n = scanner.nextLine();
-                record.setA2(n);
-                System.out.println("Antecedente 3");
-                n = scanner.nextLine();
-                record.setA3(n);
-                break;
-            case 4:
-                System.out.println("Antecedente 1");
-                n = scanner.nextLine();
-                n = scanner.nextLine();
-                record.setA1(n);
-                System.out.println("Antecedente 2");
-                n = scanner.nextLine();
-                record.setA2(n);
-                System.out.println("Antecedente 3");
-                n = scanner.nextLine();
-                record.setA3(n);
-                System.out.println("Antecedente 4");
-                n = scanner.nextLine();
-                record.setA4(n);
-                break;
-            case 5:
-                System.out.println("Antecedente 1");
-                n = scanner.nextLine();
-                n = scanner.nextLine();
-                record.setA1(n);
-                System.out.println("Antecedente 2");
-                n = scanner.nextLine();
-                record.setA2(n);
-                System.out.println("Antecedente 3");
-                n = scanner.nextLine();
-                record.setA3(n);
-                System.out.println("Antecedente 4");
-                n = scanner.nextLine();
-                record.setA4(n);
-                System.out.println("Antecedente 5");
-                n = scanner.nextLine();
-                record.setA5(n);
-                break;
-            case 6:
-                System.out.println("Antecedente 1");
-                n = scanner.nextLine();
-                n = scanner.nextLine();
-                record.setA1(n);
-                System.out.println("Antecedente 2");
-                n = scanner.nextLine();
-                record.setA2(n);
-                System.out.println("Antecedente 3");
-                n = scanner.nextLine();
-                record.setA3(n);
-                System.out.println("Antecedente 4");
-                n = scanner.nextLine();
-                record.setA4(n);
-                System.out.println("Antecedente 5");
-                n = scanner.nextLine();
-                record.setA5(n);
-                System.out.println("Antecedente 6");
-                n = scanner.nextLine();
-                record.setA6(n);
-                break;
-            case 7:
-                System.out.println("Antecedente 1");
-                n = scanner.nextLine();
-                n = scanner.nextLine();
-                record.setA1(n);
-                System.out.println("Antecedente 2");
-                n = scanner.nextLine();
-                record.setA2(n);
-                System.out.println("Antecedente 3");
-                n = scanner.nextLine();
-                record.setA3(n);
-                System.out.println("Antecedente 4");
-                n = scanner.nextLine();
-                record.setA4(n);
-                System.out.println("Antecedente 5");
-                n = scanner.nextLine();
-                record.setA5(n);
-                System.out.println("Antecedente 6");
-                n = scanner.nextLine();
-                record.setA6(n);
-                System.out.println("Antecedente 7");
-                n = scanner.nextLine();
-                record.setA7(n);
-                break;
-            case 8:
-                System.out.println("Antecedente 1");
-                n = scanner.nextLine();
-                n = scanner.nextLine();
-                record.setA1(n);
-                System.out.println("Antecedente 2");
-                n = scanner.nextLine();
-                record.setA2(n);
-                System.out.println("Antecedente 3");
-                n = scanner.nextLine();
-                record.setA3(n);
-                System.out.println("Antecedente 4");
-                n = scanner.nextLine();
-                record.setA4(n);
-                System.out.println("Antecedente 5");
-                n = scanner.nextLine();
-                record.setA5(n);
-                System.out.println("Antecedente 6");
-                n = scanner.nextLine();
-                record.setA6(n);
-                System.out.println("Antecedente 7");
-                n = scanner.nextLine();
-                record.setA7(n);
-                System.out.println("Antecedente 8");
-                n = scanner.nextLine();
-                record.setA8(n);
-                break;
-            case 9:
-                System.out.println("Antecedente 1");
-                n = scanner.nextLine();
-                n = scanner.nextLine();
-                record.setA1(n);
-                System.out.println("Antecedente 2");
-                n = scanner.nextLine();
-                record.setA2(n);
-                System.out.println("Antecedente 3");
-                n = scanner.nextLine();
-                record.setA3(n);
-                System.out.println("Antecedente 4");
-                n = scanner.nextLine();
-                record.setA4(n);
-                System.out.println("Antecedente 5");
-                n = scanner.nextLine();
-                record.setA5(n);
-                System.out.println("Antecedente 6");
-                n = scanner.nextLine();
-                record.setA6(n);
-                System.out.println("Antecedente 7");
-                n = scanner.nextLine();
-                record.setA7(n);
-                System.out.println("Antecedente 8");
-                n = scanner.nextLine();
-                record.setA8(n);
-                System.out.println("Antecedente 9");
-                n = scanner.nextLine();
-                record.setA9(n);
-                break;
-            case 10:
-                System.out.println("Antecedente 1");
-                n = scanner.nextLine();
-                n = scanner.nextLine();
-                record.setA1(n);
-                System.out.println("Antecedente 2");
-                n = scanner.nextLine();
-                record.setA2(n);
-                System.out.println("Antecedente 3");
-                n = scanner.nextLine();
-                record.setA3(n);
-                System.out.println("Antecedente 4");
-                n = scanner.nextLine();
-                record.setA4(n);
-                System.out.println("Antecedente 5");
-                n = scanner.nextLine();
-                record.setA5(n);
-                System.out.println("Antecedente 6");
-                n = scanner.nextLine();
-                record.setA6(n);
-                System.out.println("Antecedente 7");
-                n = scanner.nextLine();
-                record.setA7(n);
-                System.out.println("Antecedente 8");
-                n = scanner.nextLine();
-                record.setA8(n);
-                System.out.println("Antecedente 9");
-                n = scanner.nextLine();
-                record.setA9(n);
-                System.out.println("Antecedente 10");
-                n = scanner.nextLine();
-                record.setA10(n);
-        }
-        System.out.println("Consecuente");
-        n = scanner.nextLine();
-        record.setC(n);
+        Record record = setRule();
 
         fileManagement.overWrite(rule_number, record);
 
@@ -234,6 +37,218 @@ public class UpdateModule {
         knowledge_base = fileManagement.extract_knowledge();
         for(Record rule : knowledge_base)
             justificationModule.showRule(record);
+    }
+
+    public Record setRule() {
+        Scanner s = new Scanner(System.in);
+        int a;
+        String n = "";
+        Record record;
+
+        System.out.println("Numero de antecedentes");
+        a = s.nextInt();
+        record = new Record();
+        switch (a){
+            case 1:
+                System.out.println("Antecedente 1");
+                n = s.nextLine();
+                n = s.nextLine();
+                record.setA1(n);
+                break;
+            case 2:
+                System.out.println("Antecedente 1");
+                n = s.nextLine();
+                n = s.nextLine();
+                record.setA1(n);
+                System.out.println("Antecedente 2");
+                n = s.nextLine();
+                record.setA2(n);
+                break;
+            case 3:
+                System.out.println("Antecedente 1");
+                n = s.nextLine();
+                n = s.nextLine();
+                record.setA1(n);
+                System.out.println("Antecedente 2");
+                n = s.nextLine();
+                record.setA2(n);
+                System.out.println("Antecedente 3");
+                n = s.nextLine();
+                record.setA3(n);
+                break;
+            case 4:
+                System.out.println("Antecedente 1");
+                n = s.nextLine();
+                n = s.nextLine();
+                record.setA1(n);
+                System.out.println("Antecedente 2");
+                n = s.nextLine();
+                record.setA2(n);
+                System.out.println("Antecedente 3");
+                n = s.nextLine();
+                record.setA3(n);
+                System.out.println("Antecedente 4");
+                n = s.nextLine();
+                record.setA4(n);
+                break;
+            case 5:
+                System.out.println("Antecedente 1");
+                n = s.nextLine();
+                n = s.nextLine();
+                record.setA1(n);
+                System.out.println("Antecedente 2");
+                n = s.nextLine();
+                record.setA2(n);
+                System.out.println("Antecedente 3");
+                n = s.nextLine();
+                record.setA3(n);
+                System.out.println("Antecedente 4");
+                n = s.nextLine();
+                record.setA4(n);
+                System.out.println("Antecedente 5");
+                n = s.nextLine();
+                record.setA5(n);
+                break;
+            case 6:
+                System.out.println("Antecedente 1");
+                n = s.nextLine();
+                n = s.nextLine();
+                record.setA1(n);
+                System.out.println("Antecedente 2");
+                n = s.nextLine();
+                record.setA2(n);
+                System.out.println("Antecedente 3");
+                n = s.nextLine();
+                record.setA3(n);
+                System.out.println("Antecedente 4");
+                n = s.nextLine();
+                record.setA4(n);
+                System.out.println("Antecedente 5");
+                n = s.nextLine();
+                record.setA5(n);
+                System.out.println("Antecedente 6");
+                n = s.nextLine();
+                record.setA6(n);
+                break;
+            case 7:
+                System.out.println("Antecedente 1");
+                n = s.nextLine();
+                n = s.nextLine();
+                record.setA1(n);
+                System.out.println("Antecedente 2");
+                n = s.nextLine();
+                record.setA2(n);
+                System.out.println("Antecedente 3");
+                n = s.nextLine();
+                record.setA3(n);
+                System.out.println("Antecedente 4");
+                n = s.nextLine();
+                record.setA4(n);
+                System.out.println("Antecedente 5");
+                n = s.nextLine();
+                record.setA5(n);
+                System.out.println("Antecedente 6");
+                n = s.nextLine();
+                record.setA6(n);
+                System.out.println("Antecedente 7");
+                n = s.nextLine();
+                record.setA7(n);
+                break;
+            case 8:
+                System.out.println("Antecedente 1");
+                n = s.nextLine();
+                n = s.nextLine();
+                record.setA1(n);
+                System.out.println("Antecedente 2");
+                n = s.nextLine();
+                record.setA2(n);
+                System.out.println("Antecedente 3");
+                n = s.nextLine();
+                record.setA3(n);
+                System.out.println("Antecedente 4");
+                n = s.nextLine();
+                record.setA4(n);
+                System.out.println("Antecedente 5");
+                n = s.nextLine();
+                record.setA5(n);
+                System.out.println("Antecedente 6");
+                n = s.nextLine();
+                record.setA6(n);
+                System.out.println("Antecedente 7");
+                n = s.nextLine();
+                record.setA7(n);
+                System.out.println("Antecedente 8");
+                n = s.nextLine();
+                record.setA8(n);
+                break;
+            case 9:
+                System.out.println("Antecedente 1");
+                n = s.nextLine();
+                n = s.nextLine();
+                record.setA1(n);
+                System.out.println("Antecedente 2");
+                n = s.nextLine();
+                record.setA2(n);
+                System.out.println("Antecedente 3");
+                n = s.nextLine();
+                record.setA3(n);
+                System.out.println("Antecedente 4");
+                n = s.nextLine();
+                record.setA4(n);
+                System.out.println("Antecedente 5");
+                n = s.nextLine();
+                record.setA5(n);
+                System.out.println("Antecedente 6");
+                n = s.nextLine();
+                record.setA6(n);
+                System.out.println("Antecedente 7");
+                n = s.nextLine();
+                record.setA7(n);
+                System.out.println("Antecedente 8");
+                n = s.nextLine();
+                record.setA8(n);
+                System.out.println("Antecedente 9");
+                n = s.nextLine();
+                record.setA9(n);
+                break;
+            case 10:
+                System.out.println("Antecedente 1");
+                n = s.nextLine();
+                n = s.nextLine();
+                record.setA1(n);
+                System.out.println("Antecedente 2");
+                n = s.nextLine();
+                record.setA2(n);
+                System.out.println("Antecedente 3");
+                n = s.nextLine();
+                record.setA3(n);
+                System.out.println("Antecedente 4");
+                n = s.nextLine();
+                record.setA4(n);
+                System.out.println("Antecedente 5");
+                n = s.nextLine();
+                record.setA5(n);
+                System.out.println("Antecedente 6");
+                n = s.nextLine();
+                record.setA6(n);
+                System.out.println("Antecedente 7");
+                n = s.nextLine();
+                record.setA7(n);
+                System.out.println("Antecedente 8");
+                n = s.nextLine();
+                record.setA8(n);
+                System.out.println("Antecedente 9");
+                n = s.nextLine();
+                record.setA9(n);
+                System.out.println("Antecedente 10");
+                n = s.nextLine();
+                record.setA10(n);
+        }
+        System.out.println("Consecuente");
+        n = s.nextLine();
+        record.setC(n);
+
+        return record;
     }
 
 }
